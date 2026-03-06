cf0a51b7030f5089c41286d4e99e4fe41190ecae
srjonjoy <ghoshsrinjoy2@gmail.com>
2026-03-06 09:29:40 +0530

    isadmin middleware also complete

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -3,52 +3,76 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config()
 
-export const authMiddleware=async(req,res,next)=>{
-   try {
-    console.log(req.cookies);
-    
-     const token=req.cookies.jwt
-       if(!token){
+export const authMiddleware = async (req, res, next) => {
+    try {
+        console.log(req.cookies);
+
+        const token = req.cookies.jwt
+        if (!token) {
             return res.status(401).json({
-                message:"Unauthorized no token provided"
+                message: "Unauthorized no token provided"
             })
         }
         let decode;
         try {
-            decode=jwt.verify(token,process.env.JWT_SECRET)
+            decode = jwt.verify(token, process.env.JWT_SECRET)
             console.log(decode);
-            
-            
+
+
         } catch (error) {
             return res.status(401).json({
-                message:"Unauthorized invalid token"
+                message: "Unauthorized invalid token"
             })
-            
+
         }
-        const user=await prisma.user.findUnique({
-             where:{id:decode.id},
-            select:{
-                id:true,
-                
-                name:true,
-                email:true,
-                role:true
+        const user = await prisma.user.findUnique({
+            where: { id: decode.id },
+            select: {
+                id: true,
+
+                name: true,
+                email: true,
+                role: true
             }
         })
-         if(!user){
+        if (!user) {
             return res.status(401).json({
-                message:"Unauthorized user not found"
+                message: "Unauthorized user not found"
             })
         }
-        req.user=user
+        req.user = user
         next()
-     
-   } catch (error) {
-    console.log("error in auth middleware",error);
-        res.status(500).json({
-            message:"some problem occured "
-        })
-    
-   }
-}
 
+    } catch (error) {
+        console.log("error in auth middleware", error);
+        res.status(500).json({
+            message: "some problem occured "
+        })
+
+    }
+}
+// checkadmin middlwware
+export const isAdmin = async (req, res, next) => {
+    try {
+        const userId = req.user.id
+        const user = await prisma.user.findUnique({
+            where: { id: userId },
+            select: {
+                role: true
+            }
+        })
+        if (!user || user.role !== 'ADMIN') {
+            return res.status(403).json({
+                message: "Unauthorized user is not admin"
+            })
+
+        }
+        next()
+    } catch (error) {
+        console.log("error in auth admin", error);
+        res.status(500).json({
+            message: "some problem occured "
+        })
+
+    }
+}
